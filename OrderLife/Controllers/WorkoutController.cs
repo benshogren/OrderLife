b5099d76642465277dd35fb8a3db4bc751226342
Ben Shogren <benshogren@gmail.com>
2012-10-05 16:10:11 -0700

    New Workout model, and some new texting.

---
+++ OrderLife/Controllers/WorkoutController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrderLife.Models;
+
+namespace OrderLife.Controllers
+{ 
+    public class WorkoutController : Controller
+    {
+        private WorkoutDesciptionDBContext db = new WorkoutDesciptionDBContext();
+
+        //
+        // GET: /Workout/
+
+        public ViewResult Index()
+        {
+            return View(db.WorkoutDescription.ToList());
+        }
+
+        //
+        // GET: /Workout/Details/5
+
+        public ViewResult Details(int id)
+        {
+            WorkoutDescription workoutdescription = db.WorkoutDescription.Find(id);
+            return View(workoutdescription);
+        }
+
+        //
+        // GET: /Workout/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Workout/Create
+
+        [HttpPost]
+        public ActionResult Create(WorkoutDescription workoutdescription)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WorkoutDescription.Add(workoutdescription);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(workoutdescription);
+        }
+        
+        //
+        // GET: /Workout/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            WorkoutDescription workoutdescription = db.WorkoutDescription.Find(id);
+            return View(workoutdescription);
+        }
+
+        //
+        // POST: /Workout/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(WorkoutDescription workoutdescription)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(workoutdescription).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(workoutdescription);
+        }
+
+        //
+        // GET: /Workout/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            WorkoutDescription workoutdescription = db.WorkoutDescription.Find(id);
+            return View(workoutdescription);
+        }
+
+        //
+        // POST: /Workout/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            WorkoutDescription workoutdescription = db.WorkoutDescription.Find(id);
+            db.WorkoutDescription.Remove(workoutdescription);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OrderLife/Models/WorkoutDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OrderLife.Models
+{
+    public class WorkoutDescription
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Description { get; set; }
+    }
+    public class WorkoutDesciptionDBContext : DbContext
+    {
+        public DbSet<WorkoutDescription> WorkoutDescription { get; set; }
+    }
+}
